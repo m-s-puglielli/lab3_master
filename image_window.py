705f0fe4fa33b4237c65426444358384ad798a65
m-s-puglielli <pugliell21@up.edu>
2020-02-17 15:17:06 -0800

    Maximilian: initial commit

--- image_window.py
+++ image_window.py
@@ -3,10 +3,13 @@ import cv2
 def main():
     cv2.namedWindow("Create View", flags=cv2.WINDOW_AUTOSIZE)
     
-    stream = cv2.VideoCapture("http://raspberrypi-1.local:8081")
-    
+    stream = cv2.VideoCapture("http://raspberrypi-2.local:8081")
+
     while True:
-        img = []
-        stream.read(img)
-        cv2.imshow("Create View",img)
+        #img = []
+        #stream.read(img)
+        ret, frame = stream.read()
+        cv2.imshow("Create View",frame)
         cv2.waitKey(5)
+
+main()
