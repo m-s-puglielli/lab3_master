7d84e82cc418ca6c70acd6e3dbf168bc486aa63b
m-s-puglielli <pugliell21@up.edu>
2020-02-27 13:59:46 -0800

    I fixed some bugs with printing responses, where the response was not decoded before being printed.
    I changed a function call from "fromstring()", which is depricated, to "frombuffer().
    
    I wrote all of the states for the statemachine execpt for SEARCH.

--- lab3_maximilian.py
+++ lab3_maximilian.py
@@ -15,6 +15,7 @@ import urllib.request
 import cv2
 import numpy
 import copy
+from random import randint
 
 socketLock = threading.Lock()
 imageLock = threading.Lock()
@@ -24,9 +25,9 @@ IP_ADDRESS = "192.168.1.103"
 
 class States(enum.Enum):
 	SEARCH    = enum.auto()
-	S_LEFT    = enum.auto()
-	S_RIGHT   = enum.auto()
-	M_FORWARD = enum.auto()
+	TURN_LEFT    = enum.auto()
+	TURN_RIGHT   = enum.auto()
+	MOVE_FORWARD = enum.auto()
 
 class StateMachine(threading.Thread):
 
@@ -39,7 +40,7 @@ class StateMachine(threading.Thread):
 		self.IP_ADDRESS = IP_ADDRESS
 		self.CONTROLLER_PORT = 5001
 		self.TIMEOUT = 10   # If its unable to connect after 10 seconds, give up.  Want this to be a while so robot can init.
-		self.STATE = States.LISTEN
+		self.STATE = States.SEARCH
 		self.RUNNING = True
 		self.DIST = False
 		self.video = ImageProc()
@@ -50,7 +51,7 @@ class StateMachine(threading.Thread):
 		# CONNECT TO THE MOTORCONTROLLER
 		try:
 			with socketLock:
-				self.sock = socket.create_connection( (self.IP_ADDRESS, self.CONTROLLER_PORT), self.TIMEOUT)
+				self.sock = socket.create_connection((self.IP_ADDRESS, self.CONTROLLER_PORT), self.TIMEOUT)
 				self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 			print("Connected to RP")
 		except Exception as e:
@@ -62,9 +63,9 @@ class StateMachine(threading.Thread):
 		with socketLock:
 			self.sock.sendall("i /dev/ttyUSB0".encode())
 			print("Sent command")
-			result = self.sock.recv(128)
+			result = self.sock.recv(128).decode()
 			print(result)
-			if result.decode() != "i /dev/ttyUSB0":
+			if result != "i /dev/ttyUSB0":
 				self.RUNNING = False
 
 		self.sensors = Sensing(self.sock)
@@ -78,18 +79,47 @@ class StateMachine(threading.Thread):
 	# BEGINNING OF THE CONTROL LOOP
 	def run(self):
 		while(self.RUNNING):
-			if   self.STATE == States.SEARCH:
-				pass
-			elif self.STATE == States.S_LEFT:
-				pass
-			elif self.STATE == States.S_RIGHT:
-				pass
-			elif self.STATE == States.M_FORWARD:
-				pass
+			sleep(0.01)
+			if self.STATE == States.SEARCH:
+				rnd = randint(1, 3)
+				if rnd == 1:
+					self.STATE = States.TURN_LEFT
+				elif rnd == 2:
+					self.STATE = States.TURN_RIGHT
+				elif rnd == 3:
+					self.STATE = States.MOVE_FORWARD
+				else:
+					print("ERROR: rnd is corrupted")
+
+			elif self.STATE == States.TURN_LEFT:
+				self.sock.sendall("a drive_direct(500, -500)".encode())
+				discard = self.sock.recv(128).decode()
+				sleep(0.05)
+				self.sock.sendall("a drive_straight(0)".encode())
+				discard = self.sock.recv(128).decode()
+				self.STATE = States.SEARCH
+
+			elif self.STATE == States.TURN_RIGHT:
+				self.sock.sendall("a drive_direct(-500, 500)".encode())
+				discard = self.sock.recv(128).decode()
+				sleep(0.05)
+				self.sock.sendall("a drive_straight(0)".encode())
+				discard = self.sock.recv(128).decode()
+				self.STATE = States.SEARCH
+
+			elif self.STATE == States.MOVE_FORWARD:
+				self.sock.sendall("a drive_direct(500, 500)".encode())
+				discard = self.sock.recv(128).decode()
+				sleep(0.5)
+				self.sock.sendall("a drive_straight(0)".encode())
+				discard = self.sock.recv(128).decode()
+				self.STATE = States.SEARCH
+
 			else:
 				print("ERROR: self.STATE is corrupted")
 
 	# END OF CONTROL LOOP
+
 
 		# STOP ANY OTHER THREADS TALKING TO THE ROBOT
 		self.sensors.RUNNING = False
@@ -106,14 +136,14 @@ class StateMachine(threading.Thread):
 		"""
 		with socketLock:
 			self.sock.sendall("c".encode())
-			print(self.sock.recv(128))
+			print(self.sock.recv(128).decode() + "\n")
 			self.sock.close()
 
 		# If the user didn't request to halt, we should stop listening anyways
 		self.listener.stop()
 
-#        self.sensors.join()
-#        self.video.join()
+#		self.sensors.join()
+#		self.video.join()
 
 	def on_press(self, key):
 		# NOTE: DO NOT attempt to use the socket directly from here
@@ -125,15 +155,15 @@ class StateMachine(threading.Thread):
 	def on_release(self, key):
 		# NOTE: DO NOT attempt to use the socket directly from here
 		print('{0} released'.format(key))
-		if	key == keyboard.Key.esc or\
-			key == keyboard.Key.ctrl:
+		if key == keyboard.Key.esc:
 			# STOP LISTENER
-			self.RUNNING = False
-			self.sensors.RUNNING = False
-			self.video.RUNNING = False
+			self.RUNNING			= False
+			self.sensors.RUNNING	= False
+			self.video.RUNNING		= False
 			return False
 
 # END OF STATEMACHINE
+
 
 
 class Sensing(threading.Thread):
@@ -151,9 +181,10 @@ class Sensing(threading.Thread):
 			# You can change the polling frequency to optimize performance, don't forget to use socketLock
 			with socketLock:
 				self.sock.sendall("a distance".encode())
-				print(self.sock.recv(128))
+				print("distance = {0}\n".format(self.sock.recv(128).decode()))
 
 # END OF SENSING
+
 
 
 class ImageProc(threading.Thread):
@@ -166,9 +197,9 @@ class ImageProc(threading.Thread):
 		self.RUNNING = True
 		self.latestImg = []
 		self.feedback = []
-		self.thresholds = {'low_hue':         25, 'high_hue':         35,\
-						   'low_saturation': 100, 'high_saturation': 235,\
-						   'low_value':      125, 'high_value':      230}
+		self.thresholds = {'low_hue':			25,		'high_hue':			35,\
+						   'low_saturation':	100,	'high_saturation':	235,\
+						   'low_value':			125,	'high_value':		230}
 
 	def run(self):
 		url = "http://"+self.IP_ADDRESS+":"+str(self.PORT)
@@ -189,7 +220,7 @@ class ImageProc(threading.Thread):
 #                    bytes = bytes[b+2:]
 #                    print("found image", a, b, len(bytes))
 					break
-			img = cv2.imdecode(numpy.fromstring(jpg, dtype=numpy.uint8),cv2.IMREAD_COLOR)
+			img = cv2.imdecode(numpy.frombuffer(jpg, dtype=numpy.uint8),cv2.IMREAD_COLOR)
 			# Resize to half size so that image processing is faster
 			img = cv2.resize(img, ((int)(len(img[0])/4),(int)(len(img)/4)))
 
@@ -216,12 +247,17 @@ class ImageProc(threading.Thread):
 
 		for y in range(len(hsv_img)):
 			for x in range(len(hsv_img[0])):
-				if self.thresholds['low_hue']        <= hsv_img[y][x][0] and hsv_img[y][x][0] <= self.thresholds['high_hue']        and\
-				   self.thresholds['low_saturation'] <= hsv_img[y][x][1] and hsv_img[y][x][1] <= self.thresholds['high_saturation'] and\
-				   self.thresholds['low_value']      <= hsv_img[y][x][2] and hsv_img[y][x][2] <= self.thresholds['high_value']:
+				if	self.thresholds['low_hue']        <= hsv_img[y][x][0] and hsv_img[y][x][0] <= self.thresholds['high_hue']        and\
+					self.thresholds['low_saturation'] <= hsv_img[y][x][1] and hsv_img[y][x][1] <= self.thresholds['high_saturation'] and\
+					self.thresholds['low_value']      <= hsv_img[y][x][2] and hsv_img[y][x][2] <= self.thresholds['high_value']:
+					imgToModify[y][x][0] = 255
+					imgToModify[y][x][1] = 255
+					imgToModify[y][x][2] = 255
+				else:
 					imgToModify[y][x][0] = 0
 					imgToModify[y][x][1] = 0
-					imgToModify[y][x][2] = 255
+					imgToModify[y][x][2] = 0
+
 #				if self.thresholds['low_blue'] <= self.latestImg[y,x][0] and self.latestImg[y,x][0] <= self.thresholds['high_blue']:
 #					imgToModify[y,x][0] = 255
 #					imgToModify[y,x][1] = 0
@@ -234,9 +270,9 @@ class ImageProc(threading.Thread):
 #					imgToModify[y,x][0] = 0
 #					imgToModify[y,x][1] = 0
 #					imgToModify[y,x][2] = 255
-		# TODO: Work here
 
 # END OF IMAGEPROC
+
 
 
 if __name__ == "__main__":
@@ -253,12 +289,12 @@ if __name__ == "__main__":
 	sm.start()
 
 	# Probably safer to do this on the main thread rather than in ImgProc init
-	cv2.createTrackbar('low_hue',         'sliders', sm.video.thresholds['low_hue'],         255, lambda x: sm.video.setThresh('low_hue',         x))
-	cv2.createTrackbar('high_hue',        'sliders', sm.video.thresholds['high_hue'],        255, lambda x: sm.video.setThresh('high_hue',        x))
-	cv2.createTrackbar('low_saturation',  'sliders', sm.video.thresholds['low_saturation'],  255, lambda x: sm.video.setThresh('low_saturation',  x))
-	cv2.createTrackbar('high_saturation', 'sliders', sm.video.thresholds['high_saturation'], 255, lambda x: sm.video.setThresh('high_saturation', x))
-	cv2.createTrackbar('low_value',       'sliders', sm.video.thresholds['low_value'],       255, lambda x: sm.video.setThresh('low_value',       x))
-	cv2.createTrackbar('high_value',      'sliders', sm.video.thresholds['high_value'],      255, lambda x: sm.video.setThresh('high_value',      x))
+	cv2.createTrackbar('low_hue',			'sliders', sm.video.thresholds['low_hue'],			255, lambda x: sm.video.setThresh('low_hue',			x))
+	cv2.createTrackbar('high_hue',			'sliders', sm.video.thresholds['high_hue'],			255, lambda x: sm.video.setThresh('high_hue',			x))
+	cv2.createTrackbar('low_saturation',	'sliders', sm.video.thresholds['low_saturation'],	255, lambda x: sm.video.setThresh('low_saturation',		x))
+	cv2.createTrackbar('high_saturation',	'sliders', sm.video.thresholds['high_saturation'],	255, lambda x: sm.video.setThresh('high_saturation',	x))
+	cv2.createTrackbar('low_value',			'sliders', sm.video.thresholds['low_value'],		255, lambda x: sm.video.setThresh('low_value',			x))
+	cv2.createTrackbar('high_value',		'sliders', sm.video.thresholds['high_value'],		255, lambda x: sm.video.setThresh('high_value',			x))
 
 	while len(sm.video.latestImg) == 0:
 		sleep(1)
@@ -273,4 +309,4 @@ if __name__ == "__main__":
 
 	sleep(1)
 
-#    sm.join()
+#	sm.join()
