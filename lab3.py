22763fe867e9119490cb14b6daab40579d4f0274
liao21 <liao21@up.edu>
2020-03-09 17:11:37 -0700

    Update lab3_notes.txt

--- lab3.py
+++ lab3.py
@@ -6,6 +6,8 @@ Lab #3: Computer Vision & General Suffering
 """
 
 import socket
+from time import *
+from pynput import keyboard
 import sys
 import threading
 import enum
@@ -13,8 +15,6 @@ import urllib.request
 import cv2
 import numpy
 import copy
-import time
-from pynput import keyboard
 from random import randint
 
 socketLock	= threading.Lock()
@@ -79,7 +79,7 @@ class StateMachine(threading.Thread):
 	# BEGINNING OF THE CONTROL LOOP
 	def run(self):
 		while(self.RUNNING):
-			time.sleep(0.01)
+			sleep(0.01)
 			if self.STATE == States.SEARCH:
 				pass
 				#rnd = randint(1, 3)
@@ -95,7 +95,7 @@ class StateMachine(threading.Thread):
 			elif self.STATE == States.TURN_LEFT:
 				self.sock.sendall("a drive_direct(500, -500)".encode())
 				discard = self.sock.recv(128).decode()
-				time.sleep(0.05)
+				sleep(0.05)
 				self.sock.sendall("a drive_straight(0)".encode())
 				discard = self.sock.recv(128).decode()
 				self.STATE = States.SEARCH
@@ -103,7 +103,7 @@ class StateMachine(threading.Thread):
 			elif self.STATE == States.TURN_RIGHT:
 				self.sock.sendall("a drive_direct(-500, 500)".encode())
 				discard = self.sock.recv(128).decode()
-				time.sleep(0.05)
+				sleep(0.05)
 				self.sock.sendall("a drive_straight(0)".encode())
 				discard = self.sock.recv(128).decode()
 				self.STATE = States.SEARCH
@@ -111,7 +111,7 @@ class StateMachine(threading.Thread):
 			elif self.STATE == States.MOVE_FORWARD:
 				self.sock.sendall("a drive_direct(500, 500)".encode())
 				discard = self.sock.recv(128).decode()
-				time.sleep(0.5)
+				sleep(0.5)
 				self.sock.sendall("a drive_straight(0)".encode())
 				discard = self.sock.recv(128).decode()
 				self.STATE = States.SEARCH
@@ -127,7 +127,7 @@ class StateMachine(threading.Thread):
 		self.video.RUNNING = False
 
 		# WAIT FOR THREADS TO FINISH
-		time.sleep(1)
+		sleep(1)
 
 		# NEED TO DISCONNECT
 		"""
@@ -176,7 +176,7 @@ class Sensing(threading.Thread):
 
 	def run(self):
 		while self.RUNNING:
-			time.sleep(0.1)
+			sleep(0.1)
 			# This is where I would get a sensor update
 			# Store it in this class
 			# You can change the polling frequency to optimize performance, don't forget to use socketLock
@@ -205,16 +205,16 @@ class ImageProc(threading.Thread):
 		# 					'low_value':      0, 'high_value':       255}
 
 		# tennisball in the morning
-		self.thresholds = {	'low_hue':			0,		'high_hue':			66,
-							'low_saturation':	0,		'high_saturation':	111,
-							'low_value':		221,	'high_value':		255	}
+		self.thresholds = {	'low_hue':			30, 'high_hue':			56,
+							'low_saturation':	28, 'high_saturation':	160,
+							'low_value':		84, 'high_value':		255	}
 
 
 	def run(self):
 		url = "http://"+self.IP_ADDRESS+":"+str(self.PORT)
 		stream = urllib.request.urlopen(url)
 		while(self.RUNNING):
-#			time.sleep(0.5)
+#			sleep(0.5)
 			bytes = b''
 			while self.RUNNING:
 				# Image size is about 40k bytes, so this loops about 5 times
@@ -254,10 +254,11 @@ class ImageProc(threading.Thread):
 			# after eroding the image you can see the update in feedback_filtered
 			with imageLock:
 				self.feedback_filtered = copy.deepcopy(img)
-				self.erode(self.feedback_filtered, 2)
-				self.dilate_big(self.feedback_filtered, 2, 2)
-				cv2.GaussianBlur(self.feedback_filtered, (11, 11), 0)
-				self.blob_detection(self.feedback_filtered)
+				self.feedback_filtered = self.dilate_big(self.feedback_filtered, 2, 2)
+				self.feedback_filtered = self.erode(self.feedback_filtered, 2)
+
+			# Track Circles
+			self.track_circles(self.feedback_filtered)
 
 
 
@@ -269,12 +270,14 @@ class ImageProc(threading.Thread):
 
 	# if a pixel is not interesting, make all surrounding pixels not interesting
 	# white, (255,255,255) is "interesting", black is not
-	def erode(self, imgToModify, scale):
-		for y in range(1, len(imgToModify)-1, scale):
-			for x in range(1, len(imgToModify[0])-1, scale):
-				if	imgToModify[y][x][0] == 0 and\
-					imgToModify[y][x][1] == 0 and\
-					imgToModify[y][x][2] == 0:
+	def erode(self, original, scale):
+		imgToModify = copy.deepcopy(original)
+
+		for y in range(1, len(original)-1, scale):
+			for x in range(1, len(original)-1, scale):
+				if	original[y][x][0] == 0 and\
+					original[y][x][1] == 0 and\
+					original[y][x][2] == 0:
 					for i in range(3):
 						imgToModify [y - 1]	[x - 1]	[i] = 0
 						imgToModify [y - 1]	[x]		[i] = 0
@@ -284,85 +287,74 @@ class ImageProc(threading.Thread):
 						imgToModify [y + 1]	[x - 1]	[i] = 0
 						imgToModify [y + 1]	[x]		[i] = 0
 						imgToModify [y + 1]	[x + 1]	[i] = 0
-
-#	def erode_big(self, original, scale, how_big):
-#		imgToModify = original
-#
-#		for y in range(how_big, len(original)-how_big, scale):
-#			for x in range(how_big, len(original)-how_big, scale):
-#				if	original[y][x][0] == 0 and\
-#					original[y][x][1] == 0 and\
-#					original[y][x][2] == 0:
-#					for i in range(3):
-#						for j in range(y-how_big, y+how_big):
-#							for k in range(x-how_big, x+how_big):
-#								imgToModify[j][k][i] = 0
 #		self.feedback_filtered = imgToModify
+		return copy.deepcopy(imgToModify)
+
+
+	def erode_big(self, original, scale, how_big):
+		imgToModify = original
+
+		for y in range(how_big, len(original)-how_big, scale):
+			for x in range(how_big, len(original)-how_big, scale):
+				if	original[y][x][0] == 0 and\
+					original[y][x][1] == 0 and\
+					original[y][x][2] == 0:
+					for i in range(3):
+						for j in range(y-how_big, y+how_big):
+							for k in range(x-how_big, x+how_big):
+								imgToModify[j][k][i] = 0
+		self.feedback_filtered = imgToModify
 
 
 	# if a pixel is interesting, make all surrounding pixels interesting
 	# white, (255,255,255) is "interesting", black is not
-#	def dilate(self, original, scale):
-#		imgToModify = original
-#
-#		for y in range(1, len(original)-1, scale):
-#			for x in range(1, len(original)-1, scale):
-#				if	original[y][x][0] == 255 and\
-#					original[y][x][1] == 255 and\
-#					original[y][x][2] == 255:
-#					for i in range(3):
-#						imgToModify [y - 1]	[x - 1]	[i] = 255
-#						imgToModify [y - 1]	[x]		[i] = 255
-#						imgToModify [y - 1]	[x + 1]	[i] = 255
-#						imgToModify [y]		[x - 1]	[i] = 255
-#						imgToModify [y]		[x + 1]	[i] = 255
-#						imgToModify [y + 1]	[x - 1]	[i] = 255
-#						imgToModify [y + 1]	[x]		[i] = 255
-#						imgToModify [y + 1]	[x + 1]	[i] = 255
-#		self.feedback_filtered = imgToModify
+	def dilate(self, original, scale):
+		imgToModify = original
 
-	def dilate_big(self, imgToModify, scale, how_big):
-		for y in range(how_big, len(imgToModify)-how_big, scale):
-			for x in range(how_big, len(imgToModify[0])-how_big, scale):
-				if	imgToModify[y][x][0] == 255 and\
-					imgToModify[y][x][1] == 255 and\
-					imgToModify[y][x][2] == 255:
+		for y in range(1, len(original)-1, scale):
+			for x in range(1, len(original)-1, scale):
+				if	original[y][x][0] == 255 and\
+					original[y][x][1] == 255 and\
+					original[y][x][2] == 255:
 					for i in range(3):
-						for j in range(y - how_big, y + how_big):
-							for k in range(x - how_big, x + how_big):
+						imgToModify [y - 1]	[x - 1]	[i] = 255
+						imgToModify [y - 1]	[x]		[i] = 255
+						imgToModify [y - 1]	[x + 1]	[i] = 255
+						imgToModify [y]		[x - 1]	[i] = 255
+						imgToModify [y]		[x + 1]	[i] = 255
+						imgToModify [y + 1]	[x - 1]	[i] = 255
+						imgToModify [y + 1]	[x]		[i] = 255
+						imgToModify [y + 1]	[x + 1]	[i] = 255
+		self.feedback_filtered = imgToModify
+
+	def dilate_big(self, original, scale, how_big):
+		imgToModify = copy.deepcopy(original)
+
+		for y in range(how_big, len(original)-how_big, scale):
+			for x in range(how_big, len(original)-how_big, scale):
+				if	original[y][x][0] == 255 and\
+					original[y][x][1] == 255 and\
+					original[y][x][2] == 255:
+					for i in range(3):
+						for j in range(y-how_big, y+how_big):
+							for k in range(x-how_big, x+how_big):
 								imgToModify[j][k][i] = 255
+#		self.feedback_filtered = imgToModify
+		return copy.deepcopy(imgToModify)
 
-	# CIRCLE TRACKING
-#	def track_circles(self, imgToModify):
-#		gray = cv2.cvtColor(imgToModify, cv2.COLOR_BGR2GRAY)
-#		circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
-#		if circles is not None:
-#			circles = np.round(circles[0, :]).astype("int")
-#			for (x, y, r) in circles:
-#				print("Circle at {0}, {1}, with radius {2}".format(x, y, r))
-#				cv2.circle(imgToModify, (x, y), r, (0, 255, 0), 4)
-#				cv2.rectangle(imgToModify, (x - 5, y - 5), (x + 5, y + 5), (0, 0, 255), -1)
-#		else:
-#			print("CIRCLE IS NONE!!!")
+		# if no pixels are changed
+		#	set all pink pixels to interesting
 
-	# BLOB DETECTION
-	def blob_detection(self, imgToModify):
-		ret, blobs = cv2.threshold(imgToModify, 127, 255, 0)
-		blobs = cv2.findContours(imgToModify, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-		blobs = imutils.grab_contours(blobs)
-		# only proceed if at least one contour was found
-		if (len(blobs) > 0):
-			# find the largest contour in the mask, then use
-			# it to compute the minimum enclosing circle and
-			# centroid
-			c = max(blobs, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-			print("center = {0}".format(center))
-			cv2.circle(imgToModify, center, radius, (0, 255, 0), 4)
-
-
+	def track_circles(self, imgToModify):
+		# CIRCLE TRACKING
+		gray = cv2.cvtColor(imgToModify, cv2.COLOR_BGR2GRAY)
+		circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+		if circles is not None:
+			circles = np.round(circles[0, :]).astype("int")
+			for (x, y, r) in circles:
+				print("Circle at {0}, {1}, with radius {2}".format(x, y, r))
+				cv2.circle(imgToModify, (x, y), r, (0, 255, 0), 4)
+				cv2.rectangle(imgToModify, (x - 5, y - 5), (x + 5, y + 5), (0, 0, 255), -1)
 
 	def doImgProc(self, imgToModify):
 #		pixel = self.latestImg[120,160]
@@ -458,7 +450,7 @@ if __name__ == "__main__":
 	cv2.createTrackbar('high_value',		'sliders', sm.video.thresholds['high_value'],		255, lambda x: sm.video.setThresh('high_value',			x))
 
 	while len(sm.video.latestImg) == 0:
-		time.sleep(1)
+		sleep(1)
 
 	while(sm.RUNNING):
 		with imageLock:
@@ -469,6 +461,6 @@ if __name__ == "__main__":
 
 	cv2.destroyAllWindows()
 
-	time.sleep(1)
+	sleep(1)
 
 #	sm.join()
